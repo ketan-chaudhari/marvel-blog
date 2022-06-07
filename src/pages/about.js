826ebeb24bd6ddb71ca1d3b710af7f886378f4f3
Ketan Chaudhari <ketan.171286@gmail.com>
2022-06-07 13:37:20 +1000

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
 
+const pageContainer = {
+    width: 1200,
+    minHeight: 500,
+    
+}
+
+/*
 export default function About()
 {
     return(
@@ -11,8 +18,19 @@ export default function About()
     )
 
 }
+*/
 
 const AboutPage = () => {
 
-    
+    return(
+        <main style={pageContainer}>
+        <title>About Me</title>
+        <h1>About Me</h1>
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <Link to="/">Back to Home</Link>
+        </main>
+    )
 }
+
+// Step 3: Export your component
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
+// Step 1: Import React
 import * as React from "react";
 import { Link } from "gatsby";
 
 // styles
+
+const pageContainer = {
+    width: 1200,
+    minHeight: 500,
+    
+}
 const pageStyles = {
   color: "#232129",
   padding: 96,
@@ -127,10 +134,12 @@ const links = [
   },
 ]
 
-// markup
+
+// Step 2: Define your component
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
+      <div style={pageContainer}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
         Welcome Ketan !!
@@ -144,8 +153,9 @@ const IndexPage = () => {
         </span>
       </p>
       <p style={paragraphStyles}>
-        <Link to='about' style={docLinkStyle}>About Us</Link>
+        <Link to='/about' style={docLinkStyle}>About Us</Link>
       </p>
+
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
@@ -174,12 +184,19 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
+
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>This is the home page content</p>
+        
+
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
+      </div>
     </main>
   )
 }
 
+// Step 3: Export your component
 export default IndexPage
