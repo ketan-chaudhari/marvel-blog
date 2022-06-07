e5f4f0cf2b115bf0f1024f477eba08c195088682
Ketan Chaudhari <ketan.171286@gmail.com>
2022-06-07 11:32:20 +1000

    upload all changes

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function About()
+{
+    return(
+        <div>
+            <h1>About Us</h1>
+            <Link to="/">Back to Home</Link>
+        </div>
+    )
+
+}
+
+const AboutPage = () => {
+
+    
+}
